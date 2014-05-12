7c888386972ecf4a93a4bfcc2340c486cfb48647
Lai Duy Thanh <thanhlai@outlook.com>
2014-05-11 17:16:57 -0700

    Added basic routes and controllers and Admin area

--- SimpleBlog/App_Start/RouteConfig.cs
+++ SimpleBlog/App_Start/RouteConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleBlog.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,11 +14,13 @@ namespace SimpleBlog
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            var namespaces = new[] { typeof(PostsController).Namespace };
+
+            //learn how to write my own route
+            //name of the route
+            routes.MapRoute("Home", "", new { controller = "Posts", action = "Index" }, namespaces);
+            //controller is like class, action is like method
+            routes.MapRoute("Login", "login", new { controller = "Auth", action = "Login" }, namespaces);
         }
     }
 }
---
+++ SimpleBlog/Areas/Admin/Controllers/PostsController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace SimpleBlog.Areas.Admin.Controllers
+{
+    public class PostsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Content("Admin Area->Post controller->index action");
+        }
+    }
+}
