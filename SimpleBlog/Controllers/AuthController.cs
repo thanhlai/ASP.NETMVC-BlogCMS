d7c14d6de259086bdd920b376a3b295d4fae640e
Lai Duy Thanh <thanhlai@outlook.com>
2014-05-14 17:17:10 -0700

    Added basic login form, Passing values between views and controllers

--- SimpleBlog/Controllers/AuthController.cs
+++ SimpleBlog/Controllers/AuthController.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using SimpleBlog.ViewModels;
 namespace SimpleBlog.Controllers
 {
     public class AuthController : Controller
     {
         public ActionResult Login() //name of the razor file cshtml
         {
-            return View();
+            return View(new AuthLogin { 
+            });
+        }
+        [HttpPost]
+        public ActionResult Login(AuthLogin form)
+        {
+            if (!ModelState.IsValid)
+                return View(form);
+            if (form.Username != "thanh")
+            {
+                ModelState.AddModelError("Username", "Username is not the admin!");
+                return View(form);
+            }
+            return Content("Form is valid");
         }
 	}
 }
---
+++ SimpleBlog/ViewModels/Auth.cs
@@ -0,0 +1,15 @@
+﻿//ViewModels: good practice
+//defines the contract between Views And Controllers
+using System.ComponentModel.DataAnnotations;
+namespace SimpleBlog.ViewModels
+{
+    public class AuthLogin
+    {
+        //send from Views to Controllers
+        [Required]
+        public string Username { get; set; }
+
+        [DataType(DataType.Password), Required]
+        public string Password { get; set; }
+    }
+}
