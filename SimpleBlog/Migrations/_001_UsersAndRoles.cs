5216b31c553a127fcee8d68b6ad588dd9304d9aa
Lai Duy Thanh <thanhlai@outlook.com>
2014-05-17 20:40:39 -0700

    Added FluentMigrator and MySQL

---
+++ SimpleBlog/Migrations/_001_UsersAndRoles.cs
@@ -0,0 +1,38 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace SimpleBlog.Migrations
+{
+    [Migration(1)]
+    public class _001_UsersAndRoles : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("users")
+                .WithColumn("id").AsInt32().Identity().PrimaryKey()
+                .WithColumn("username").AsString(128)
+                .WithColumn("email").AsCustom("VARCHAR(256)")
+                .WithColumn("password").AsString(128);
+
+            Create.Table("roles")
+                .WithColumn("id").AsInt32().Identity().PrimaryKey()
+                .WithColumn("name").AsString(128);
+
+            Create.Table("role_users")
+                .WithColumn("user_id").AsInt32().ForeignKey("users", "id").OnDelete(Rule.Cascade)
+                .WithColumn("role_id").AsInt32().ForeignKey("roles", "id").OnDelete(Rule.Cascade);
+        }
+
+        public override void Down()
+        {
+            Delete.Table("role_users");
+            Delete.Table("users");
+            Delete.Table("roles");
+
+        }
+    }
+}
---
+++ SimpleBlog/Tools/DeployDb-Dev.bat
@@ -0,0 +1,1 @@
+﻿..\..\packages\FluentMigrator.1.1.2.1\tools\Migrate.exe --db=mysql --target=..\bin\simpleblog.dll --configPath=..\Web.config -c=MainDb
