5ec46a69e85ff6e9ba9147e13c88e233b9dde03b
Lai Duy Thanh <thanhlai@outlook.com>
2014-05-18 22:08:00 -0700

    Fixed MVC conflict and Rollback

---
+++ SimpleBlog/Infastructure/TransactionFilter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleBlog.Infastructure
+{
+    public class TransactionFilter : IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            if (filterContext.Exception == null)
+                Database.Session.Transaction.Commit();
+            else
+                Database.Session.Transaction.Rollback();
+        }
+
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            Database.Session.BeginTransaction();
+        }
+    }
+}
