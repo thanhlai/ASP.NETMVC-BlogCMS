7a0363766b6923683430dea0e01c229432a0f1f7
Lai Duy Thanh <thanhlai@outlook.com>
2014-05-23 14:20:41 -0700

    Implement page index in the admin and post section

--- SimpleBlog/Areas/Admin/Controllers/PostsController.cs
+++ SimpleBlog/Areas/Admin/Controllers/PostsController.cs
@@ -4,15 +4,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NHibernate.Linq;
+using SimpleBlog.Models;
+using SimpleBlog.Areas.Admin.ViewModels;
 namespace SimpleBlog.Areas.Admin.Controllers
 {
     [Authorize(Roles = "admin")]
     [SelectedTab("posts")]
     public class PostsController : Controller
     {
-        public ActionResult Index()
+        private const int PostsPerPage = 5;
+
+        public ActionResult Index(int page = 1)
         {
-            return View();
+            var totalPostCount = Database.Session.Query<Post>().Count();
+            var currentPostPage = Database.Session.Query<Post>()
+                .OrderByDescending(c => c.CreateAt)
+                .Skip((page - 1) * PostsPerPage)
+                .Take(PostsPerPage)
+                .ToList();
+
+            return View(new PostsIndex 
+            { 
+                Posts = new PagedData<Post>(currentPostPage, totalPostCount, page, PostsPerPage)
+            });
         }
     }
 }
---
+++ SimpleBlog/Areas/Admin/ViewModels/Posts.cs
@@ -0,0 +1,14 @@
+﻿using SimpleBlog.Infastructure;
+using SimpleBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleBlog.Areas.Admin.ViewModels
+{
+    public class PostsIndex
+    {
+        public PagedData<Post> Posts { get; set; }
+    }
+}
