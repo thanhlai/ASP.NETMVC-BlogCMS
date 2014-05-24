ca403fcce585bb34e6d294a197854694cef6faa2
Lai Duy Thanh <thanhlai@outlook.com>
2014-05-24 10:33:36 -0700

    Created New and Edit actions for PostsController

--- SimpleBlog/Areas/Admin/Controllers/PostsController.cs
+++ SimpleBlog/Areas/Admin/Controllers/PostsController.cs
@@ -29,5 +29,62 @@ namespace SimpleBlog.Areas.Admin.Controllers
                 Posts = new PagedData<Post>(currentPostPage, totalPostCount, page, PostsPerPage)
             });
         }
+
+        public ActionResult New()
+        {
+            return View("Form", new PostsForm 
+            {
+                IsNew = true
+            });
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var post = Database.Session.Load<Post>(id);
+            if (post == null)
+                return HttpNotFound();
+
+            return View("Form", new PostsForm 
+            {
+                IsNew = false,
+                PostId = id,
+                Title = post.Title,
+                Slug = post.Slug,
+                Content = post.Content
+            });
+
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Form(PostsForm form)
+        {
+            form.IsNew = (form.PostId == null);
+            if (!ModelState.IsValid)
+                return View(form);
+
+            Post post;
+            if(form.IsNew)
+            {
+                post = new Post 
+                {
+                    CreateAt = DateTime.Now,
+                    User = Auth.User,
+                };
+            }
+            else
+            {
+                post = Database.Session.Load<Post>(form.PostId);
+                if (post == null)
+                    return HttpNotFound();
+                post.UpdateAt = DateTime.Now;
+            }
+            post.Title = form.Title;
+            post.Slug = form.Slug;
+            post.Content = form.Content;
+
+            Database.Session.SaveOrUpdate(post);
+
+            return RedirectToAction("Index");
+        }
     }
 }
--- SimpleBlog/Areas/Admin/ViewModels/Posts.cs
+++ SimpleBlog/Areas/Admin/ViewModels/Posts.cs
@@ -2,6 +2,7 @@
 using SimpleBlog.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -10,5 +11,19 @@ namespace SimpleBlog.Areas.Admin.ViewModels
     public class PostsIndex
     {
         public PagedData<Post> Posts { get; set; }
+    }
+    public class PostsForm
+    {
+        public bool IsNew { get; set; }
+        public int? PostId { get; set; }
+
+        [Required, MaxLength(128)]
+        public string Title { get; set; }
+
+         [Required, MaxLength(128)]
+        public string Slug { get; set; }
+
+        [Required, DataType(DataType.MultilineText)]
+        public string Content { get; set; }
     }
 }
